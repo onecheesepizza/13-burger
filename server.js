3f8a6a5547a424071cb51fbe4ba095af73a02d28
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2020-01-08 19:20:12 -0800

    view setup

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-const express = require('express');
+//init express
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+//static files
+app.use(express.static("public"));
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//template engine
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//import and use routes
+const routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+//start listening
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
